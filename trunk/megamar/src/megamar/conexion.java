8dbb1ac494bb6b3854f98beb41fc957552da4422
pedrolizondo@gmail.com <pedrolizondo@gmail.com@ef64602f-b943-1a42-486a-90d7d270e72f>
2012-07-02 01:22:12 +0000

    Primer commit.

---
+++ trunk/megamar/src/megamar/conexion.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package megamar;
+
+/**
+ *
+ * @author Ele
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrador
+ */
+public class conexion {
+    
+  private Connection myConnection;
+
+
+  public conexion() {
+
+    }
+
+    public void init() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost/megamar", "root", "");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public Connection getMyConnection(){
+        return myConnection;
+    }
+    
+    
+    public void close(ResultSet rs){
+        
+        if(rs !=null){
+            try{
+               rs.close();
+            }
+            catch(Exception e){}
+        
+        }
+    }
+    
+     public void close(java.sql.Statement stmt){
+        
+        if(stmt !=null){
+            try{
+               stmt.close();
+            }
+            catch(Exception e){}
+        
+        }
+    }
+     
+  public void destroy(){
+  
+    if(myConnection !=null){
+    
+         try{
+               myConnection.close();
+            }
+            catch(Exception e){}
+        
+       
+  }
+    
+  }
+}
